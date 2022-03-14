748685462174fc7128ef66d4acd0f1f73392ddc0
giubacc <giuseppe.baccini@suse.com>
2022-03-14 08:57:59 +0000

    renamed mr-plow network to vscode-mr-plow

--- .devcontainer/netcreate.sh
+++ .devcontainer/netcreate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if docker network ls | grep "mr-plow"; then
-  echo "network mr-plow already exists, nothing to do"
+if docker network ls | grep "vscode-mr-plow"; then
+  echo "network vscode-mr-plow already exists, nothing to do"
 else
-  echo "network mr-plow doesn't exist, creating ..."
-  docker network create --driver=bridge --attachable --subnet=10.70.67.0/24 mr-plow
+  echo "network vscode-mr-plow doesn't exist, creating ..."
+  docker network create --driver=bridge --attachable --subnet=10.70.67.0/24 vscode-mr-plow
 fi
